6ebaf775f9fe4b45d2e04cba777b1aa1ab5272a5
glmsr <gulumseren35@gmail.com>
2022-01-09 18:34:46 +0300

    added localStorage

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ let d = document;
 const myForm = d.getElementById('myForm');
 const booksPart = d.getElementById('booksPart');
 const sampleBook = d.getElementById('sampleBookBox');
-let cloneWastebucket;
-let count = -1;
 let deleteThis;
-let testVari;//sil
 let targetId;
-let testVariable;//sil
+let index;
+let localToInc;
+let localToDec;
+let localToBoolean;
+let localToLoop;
+let localBook;
+let loBookToObj;
+let localBookId;
+let theBookId = Math.floor(Math.random() * 9999);
+let someData;
 let cloneRemBtn;
+let cloneWastebucket;
 let cloneTitle;
 let cloneAuthor;
 let clonePage;
@@ -16,62 +23,52 @@ let cloneBtnContainer;
 let cloneDecBtn;
 let cloneIncBtn;
 let cloneReadBtn;
-let once = false;
 let cloneBook;
-let testVar;
 let myLibrary = [];
 let newName;
 let newAuthor;
 let newPage;
+let localName;
+let localAuthor;
+let localPage;
+let localRead;
 
-// VVVV CONSTRUCTOR VVVV
+
+
+  // VVVV CONSTRUCTOR VVVV
 
 class Book {
-  constructor(name, author, page, read) {
+  constructor(name, author, page, read, bookId) {
     this.name = name;
     this.author = author;
     this.page = page;
     this.read = read;
-    this.incBtn = function (n) {
-      this.page++;
-      eval(`page${n}`).textContent = `On page ${this.page}`;
-    };
-    this.decBtn = function (n) {
-      this.page--;
-      eval(`page${n}`).textContent = `On page ${this.page}`;
-    };
-    this.readBtn = function (n) {
-      if (!this.read) {
-        this.read = true;
-        eval(`page${n}`).textContent = `On page ${this.page}`;
-      } else if (this.read) {
-        eval(`page${n}`).textContent = `Read!`;
-        this.read = false;
-      }
-    };
+    this.bookId = bookId;
+
     
 
-    this.remBtn = function (n) {
-      myLibrary.splice(n, 1);
-      deleteThis = document.getElementById(`book${n}`);
-      deleteThis.parentNode.removeChild(deleteThis);
-      // console.log(deleteThis)
-    };
-
-
-    Book.prototype.info = function () {
-      console.log(`${this.name}, ${this.author}, ${this.page} pages, ${this.read}`);
-    };
-
-    Book.prototype.create = function () {
-
+    this.createFunc = function () {
+      
+      
+      
+      console.log(this.bookId);
+      this.bookId = bookId;
+      
       cloneRemBtn = d.createElement('button');
-      cloneRemBtn.id = `${count}`;
+      cloneRemBtn.id = `remBtn${bookId}`;
+      cloneRemBtn.dataset.btnId = bookId;
       cloneRemBtn.classList.add('remBtn');
       cloneRemBtn.onclick = function () {
-        targetId = event.currentTarget.id;
-        myLibrary[targetId].remBtn(targetId);
-        count--;
+        
+        deleteThis = document.getElementById(`book${this.dataset.btnId}`);
+        deleteThis.parentNode.removeChild(deleteThis);
+
+        index = myLibrary.findIndex(y => y.bookId == this.dataset.btnId);
+        myLibrary.splice(index, 1);
+        
+        
+        localStorage.remBtn(this.dataset.btnId);
+        removeFiller();
       };
 
       cloneWastebucket = d.createElement('img');
@@ -79,104 +76,196 @@ class Book {
       cloneWastebucket.src = "resources/wastebucket.png";
 
       cloneBook = d.createElement('div');
-      cloneBook.id = `book${count}`;
+      cloneBook.id = `book${bookId}`;
       cloneBook.classList.add('bookBox');
-
+      cloneBook.dataset.daBookId = theBookId;
+      
+      
       cloneTitle = d.createElement('p');
-      cloneTitle.id = `title${count}`;
+      cloneTitle.id = `title${theBookId}`;
       cloneTitle.textContent = this.name;
       cloneTitle.classList.add('title');
 
       cloneAuthor = d.createElement('p');
-      cloneAuthor.id = `author${count}`;
+      cloneAuthor.id = `author${theBookId}`;
       cloneAuthor.textContent = `By ${this.author}`;
       cloneAuthor.classList.add('author');
 
       clonePage = d.createElement('p');
-      clonePage.id = `page${count}`;
+      clonePage.id = `page${bookId}`;
       clonePage.textContent = `On page ${this.page}`;
       clonePage.classList.add('page');
 
       cloneBtnContainer = d.createElement('div');
-      cloneBtnContainer.id = `btnContainer${count}`;
+      cloneBtnContainer.id = `btnContainer${bookId}`;
       cloneBtnContainer.classList.add('buttonContainer');
 
       cloneDecBtn = d.createElement('button');
-      cloneDecBtn.id = `${count}`;
+      cloneDecBtn.id = `decBtn${bookId}`;
       cloneDecBtn.classList.add('decreasePage');
+      cloneDecBtn.dataset.btnId = bookId;
       cloneDecBtn.innerHTML = `-`;
-      cloneDecBtn.onclick = function () {
-        targetId = event.target.id;
-        myLibrary[targetId].decBtn(targetId);
+      cloneDecBtn.onclick = function (e) {
+        index = myLibrary.findIndex(y => y.bookId == bookId);
+        targetId = e.target.id;
+        myLibrary[index].decBtn(index, this.dataset.btnId);
+
+        localStorage.decBtn(this.dataset.btnId, localBookId);
       };
+    
+  
 
       cloneIncBtn = d.createElement('button');
-      cloneIncBtn.id = `${count}`;
+      cloneIncBtn.id = `incBtn${bookId}`;
       cloneIncBtn.classList.add('increasePage');
+      cloneIncBtn.dataset.btnId = bookId;
       cloneIncBtn.innerHTML = `+`;
-      cloneIncBtn.onclick = function () {
-        targetId = event.target.id;
-        myLibrary[targetId].incBtn(targetId);
-      };
+      cloneIncBtn.onclick = function (e) {
+        index = myLibrary.findIndex(y => y.bookId == this.dataset.btnId);
+        targetId = e.target.id;
+        myLibrary[index].incBtn(index, this.dataset.btnId);
+        localStorage.incBtn(this.dataset.btnId, localBookId)
+      }
 
       cloneReadBtn = d.createElement('button');
-      cloneReadBtn.id = `${count}`;
+      cloneReadBtn.id = `readBtn${bookId}`;
       cloneReadBtn.classList.add('readBtn');
       cloneReadBtn.innerHTML = `Read`;
-      cloneReadBtn.onclick = function () {
-        targetId = event.target.id;
-        myLibrary[targetId].readBtn(targetId);
-      };
-
+      cloneReadBtn.dataset.btnId = bookId;
+      cloneReadBtn.onclick = function someName(e) {
+        index = myLibrary.findIndex(y => y.bookId == this.dataset.btnId);
+        targetId = e.target.id;
+        myLibrary[index].readBtn(bookId);
+        
+        localStorage.readBtn(this.dataset.btnId);
+      }
       booksPart.appendChild(cloneBook);
       cloneRemBtn.appendChild(cloneWastebucket);
       cloneBook.append(cloneRemBtn, cloneTitle, cloneAuthor, clonePage, cloneBtnContainer);
       cloneBtnContainer.append(cloneDecBtn, cloneIncBtn, cloneReadBtn);
-
+      this.readBtn(bookId)
     };
 
+    this.incBtn = function (n, m) {
+      myLibrary[n].page++;
+      eval(`page${m}`).textContent = `On page ${this.page}`;
+    };
+    this.decBtn = function (n, m) {
+      myLibrary[n].page--;
+      eval(`page${m}`).textContent = `On page ${this.page}`;
+    };
+    this.readBtn = function (m) {
+      if (this.read) {
+        this.read = false;
+        d.getElementById(`incBtn${m}`).disabled = false;
+        d.getElementById(`decBtn${m}`).disabled = false;
+        eval(`page${m}`).textContent = `On page ${this.page}`;
+
+      } else if (!this.read) {
+        eval(`page${m}`).textContent = `Read!`;
+        d.getElementById(`incBtn${m}`).disabled = true;
+        d.getElementById(`decBtn${m}`).disabled = true;
+        this.read = true;
+        
+      };
+      
+    }
+    theBookId = Math.floor(Math.random() * 9999);
+    
+    Book.prototype.info = function () {
+      console.log(`${this.name}, ${this.author}, ${this.page} pages, ${this.read}`);
+    };
+    
   }
+}
+// VVV STORAGE PROTOTYPES VVV
+Storage.prototype.setObject = function (key, value) {
+  this.setItem(key, JSON.stringify(value));
 }
 
 
-document.getElementById('myForm').onsubmit = function () {
+Storage.prototype.getObject = function (key) {
+  var value = this.getItem(key);
+  return value && JSON.parse(value);
+}
+
+Storage.prototype.allObjects = function (h) {
+ 
+  var value = this.getItem(h);
+  return JSON.parse(value).bookId;
+  
+}
+
+Storage.prototype.remBtn = function(e){
+  for (let i = 0; i < localStorage.length; i++){
+    localToLoop = JSON.parse(this.getItem(this.key(i)));
+    if (localToLoop.bookId == e) {
+      this.removeItem(this.key(i));
+    }
+  }
+}
+
+Storage.prototype.decBtn = function (e,g) {
+  for (let i = 0; i < localStorage.length; i++) {
+    localToLoop = JSON.parse(this.getItem(this.key(i)));
+    if (localToLoop.bookId == e) { 
+      localToDec = JSON.parse(this.getItem(this.key(i)))
+      localToDec.page--;
+      this.setObject(this.key(i), localToDec);
+    }
+  }
+}
+Storage.prototype.incBtn = function (e,g) {
+  for (let i = 0; i < localStorage.length; i++) {
+    localToLoop = JSON.parse(this.getItem(this.key(i)));
+    if (localToLoop.bookId == e) { 
+      localToInc = JSON.parse(this.getItem(this.key(i)))
+      localToInc.page++;
+      this.setObject(this.key(i), localToInc);
+    }
+  }
+}
+Storage.prototype.readBtn = function (e) {
+  for (let i = 0; i < localStorage.length; i++) {
+    localToLoop = JSON.parse(this.getItem(this.key(i)));
+    if (localToLoop.bookId == e) {
+      localToBoolean = JSON.parse(this.getItem(this.key(i)));
+      if (localToBoolean.read) {
+        localToBoolean.read = false;
+        this.setObject(this.key(i), localToBoolean);
+      }
+      else if (!localToBoolean.read) {
+        localToBoolean.read = true;
+        this.setObject(this.key(i), localToBoolean);
+      }
+    }
+  }
+}
+Storage.prototype.allLocalObjects = function (h) {
+ 
+  var value = this.getItem(localStorage.key(h));
+  return JSON.parse(value).bookId;
+
+}
+
+d.getElementById('myForm').onsubmit = function () {
   newName = d.getElementById('Name').value;
   newAuthor = d.getElementById('Author').value;
   newPage = d.getElementById('Page').value;
   newRead = document.getElementById('read');
-  count++;
-  x = new Book(newName, newAuthor, newPage, true);
-  //localStorage.setItem('book', x);
-  console.log(x.info());
+  x = new Book(newName, newAuthor, newPage, true, theBookId);
+  localBookId = Math.floor(Math.random() * 99999);
+  localStorage.setObject(`${localBookId}`, x);
+  console.log(localStorage.getObject(`${theBookId}`));
+
   myLibrary.push(x);
-  x.create();
+  x.createFunc();
   removeFiller();
   return false;
-  //createBox();
-  //showBooks();
 }
 
 //  VVVV FUNCTIONS VVVV
       
-// function showBooks() {
-//   for (let i = 0; i < myLibrary.length; i++) {
-//     let k = i + 1;
-//     d.getElementById(`title${k}`).innerHTML = myLibrary[i].name;
-//     d.getElementById(`author${k}`).innerHTML = `by ${myLibrary[i].author}`;
-//     d.getElementById(`page${k}`).innerHTML = `on page ${myLibrary[i].page}`;
-//     d.getElementById(`readBtn${k}`).addEventListener('click', function() {
-//       readFunction(i);
-//         // if (myLibrary[i].read === 0) {
-//         //   myLibrary[i].read++;
-//         //   d.getElementById(`page${i + 1}`).innerHTML = `Read!`
-//         // } else if (myLibrary[i].read === 1) {
-//         //   myLibrary[i].read--;
-//         //   d.getElementById(`page${i + 1}`).innerHTML = `on page ${myLibrary[i].page}`
-//         // }
-//       })
-//       }
-//     }
-  
 // add book menu popup function
 function openForm() {
   document.getElementById("myForm").style.display = "block";
@@ -187,13 +276,34 @@ function closeForm() {
 }
 
 function removeFiller() {
-  if (myLibrary.length > 0) {
-    document.getElementById('blankFiller').style.display = 'none';
-  }
-  else if (myLibrary.length <= 0) {
+  if (myLibrary.length <= 0) {
     document.getElementById('blankFiller').style.display = 'flex';
+  }
+  else if (myLibrary.length > 0) {
+    document.getElementById('blankFiller').style.display = 'none';
   }
 }
 
+function pushLocals() {
+  for (let j = 0; j < localStorage.length; j++){
+    localBook = localStorage.getItem(localStorage.key(j));
+    loBookToObj = JSON.parse(localBook);
+    localName = loBookToObj.name;
+    localAuthor = loBookToObj.author;
+    localPage = loBookToObj.page;
+    localRead = loBookToObj.read;
+    console.log(loBookToObj.read)
+    loBookToObj = new Book(localName, localAuthor, localPage, localRead, loBookToObj.bookId);
+    myLibrary.push(loBookToObj);
+    localStorage.allLocalObjects(j);
+    console.log(loBookToObj.bookId);
+    loBookToObj.createFunc();
+  }
+}
 
-// window.addEventListener('load', function () { createBox(), showBooks() })
+function rngGenerate() {
+  theBookId = Math.floor(Math.random() * 9999);
+}
+
+window.onload = pushLocals();
+window.addEventListener('load', function () {removeFiller()})
